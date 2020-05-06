741c072394126c4f31950a4af30e33f39cebc4ac
Yixi Rao <u6826541@anu.edu.au>
2020-05-06 22:34:34 +0800

    feature 1: adding and deleting topics (rename)

--- vote/app/src/main/java/com/example/votingapp/CreateActivity.java
+++ vote/app/src/main/java/com/example/votingapp/CreateActivity.java
@@ -17,17 +17,16 @@ import java.util.LinkedList;
 
 public class CreateActivity extends AppCompatActivity {
 
-    private LinearLayout llContentView;
+    private LinearLayout lLayoutOut;
 
-    private EditText etContent1;
+    private EditText question;
 
-    private LinkedList<Button> listIBTNAdd;
+    private LinkedList<Button> AddButList;
+    private LinkedList<Button> DelButList;
 
-    private int btnIDIndex = 1000;
+    private int indexOfAdd = 1000;
 
-    private LinkedList<Button> listIBTNDel;
-
-    private int iETContentHeight = 0;
+    private int questionHeight = 0;
 
     private float fDimRatio = 1.0f;
 
@@ -35,131 +34,129 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
-        initCtrl();
+        initFirst();
     }
 
-    private void initCtrl(){
-        llContentView = (LinearLayout) this.findViewById(R.id.content_view);
-        etContent1 = (EditText) this.findViewById(R.id.et_content1);
-        listIBTNAdd = new LinkedList<Button>();
-        listIBTNDel = new LinkedList<Button>();
-        Button ibtnAdd1 = (Button) this.findViewById(R.id.ibn_add1);
-        ibtnAdd1.setOnClickListener(new View.OnClickListener() {
+    private void initFirst(){
+        lLayoutOut = (LinearLayout) this.findViewById(R.id.content_view);
+        question = (EditText) this.findViewById(R.id.et_content1);
+        AddButList = new LinkedList<Button>();
+        DelButList = new LinkedList<Button>();
+        Button addButton = (Button) this.findViewById(R.id.ibn_add1);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                iETContentHeight = etContent1.getHeight();
-                fDimRatio = iETContentHeight / 80;
+                questionHeight = question.getHeight();
+                fDimRatio = questionHeight / 80;
                 addContent(v);
             }
         });
-        listIBTNAdd.add(ibtnAdd1);
-        listIBTNDel.add(null);
+        AddButList.add(addButton);
+        DelButList.add(null);
     }
 
     private void addContent(View v) {
         if (v == null) {
             return;
         }
-
-        int iIndex = -1;
-        for (int i = 0; i < listIBTNAdd.size(); i++) {
-            if (listIBTNAdd.get(i) == v) {
-                iIndex = i;
+        int buttonIndex = -1;
+        for (int i = 0; i < AddButList.size(); i++) {
+            if (AddButList.get(i) == v) {
+                buttonIndex = i;
                 break;
             }
         }
 
-        if (iIndex >= 0) {
-            iIndex += 1;
-//--------------------------------------------------------------------------------------------------
-            LinearLayout layout = new LinearLayout(CreateActivity.this);
-            LinearLayout.LayoutParams lLayoutlayoutParams = new LinearLayout.LayoutParams(
+        if (buttonIndex >= 0) {
+            buttonIndex += 1;
+//-------------------------------------LinearLayout In-----------------------------------------------------------
+            LinearLayout lLayoutIn = new LinearLayout(CreateActivity.this);
+            LinearLayout.LayoutParams lLayoutParams = new LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            lLayoutlayoutParams.setMargins(0, (int) (fDimRatio * 5), 0, 0);
-            layout.setLayoutParams(lLayoutlayoutParams);
-            layout.setBackgroundColor(Color.argb(255, 192, 192, 192));
-            layout.setPadding((int) (fDimRatio * 5), (int) (fDimRatio * 5),
+            lLayoutParams.setMargins(0, (int) (fDimRatio * 5), 0, 0);
+            lLayoutIn.setLayoutParams(lLayoutParams);
+            lLayoutIn.setBackgroundColor(Color.argb(255, 192, 192, 192));
+            lLayoutIn.setPadding((int) (fDimRatio * 5), (int) (fDimRatio * 5),
                     (int) (fDimRatio * 5), (int) (fDimRatio * 5));
-            layout.setOrientation(LinearLayout.VERTICAL);
-//--------------------------------------------------------------------------------------------------
-            EditText etContent = new EditText(CreateActivity.this);
+            lLayoutIn.setOrientation(LinearLayout.VERTICAL);
+//--------------------------------------EditText question 1-----------------------------------------------------------
+            EditText question1 = new EditText(CreateActivity.this);
             LinearLayout.LayoutParams etParam = new LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, iETContentHeight);
-            etContent.setLayoutParams(etParam);
-            etContent.setBackgroundColor(Color.argb(255, 255, 255, 255));
-            etContent.setGravity(Gravity.LEFT);
-            etContent.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
-            etContent.setPadding((int) (fDimRatio * 5), 0, 0, 0);
-            etContent.setTextSize(16);
-            layout.addView(etContent);
-//--------------------------------------------------------------------------------------------------
-            RelativeLayout rlBtn = new RelativeLayout(CreateActivity.this);
+                    ViewGroup.LayoutParams.MATCH_PARENT, questionHeight);
+            question1.setLayoutParams(etParam);
+            question1.setBackgroundColor(Color.argb(255, 255, 255, 255));
+            question1.setGravity(Gravity.LEFT);
+            question1.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+            question1.setPadding((int) (fDimRatio * 5), 0, 0, 0);
+            question1.setTextSize(16);
+            lLayoutIn.addView(question1);
+//--------------------------------------Relative Layout-----------------------------------------------------------
+            RelativeLayout reLayout = new RelativeLayout(CreateActivity.this);
             RelativeLayout.LayoutParams rlParam = new RelativeLayout.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            rlBtn.setPadding(0, (int) (fDimRatio * 5), 0, 0);
-            rlBtn.setLayoutParams(rlParam);
-
-            Button btnAdd2 = new Button(CreateActivity.this);
-            RelativeLayout.LayoutParams btnAddParam2 =new RelativeLayout.LayoutParams(
+            reLayout.setPadding(0, (int) (fDimRatio * 5), 0, 0);
+            reLayout.setLayoutParams(rlParam);
+//--------------------------------------Button add 1 & Button delete 1-----------------------------------------------------
+            Button addButton1 = new Button(CreateActivity.this);
+            RelativeLayout.LayoutParams addButtonParam =new RelativeLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            btnAddParam2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            btnAdd2.setLayoutParams(btnAddParam2);
-            btnAdd2.setId(btnIDIndex);
-            btnAdd2.setText("ADD");
-            btnAdd2.setOnClickListener(new View.OnClickListener() {
+            addButtonParam.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            addButton1.setLayoutParams(addButtonParam);
+            addButton1.setId(indexOfAdd);
+            addButton1.setText("ADD");
+            addButton1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     addContent(v);
                 }
             });
-            rlBtn.addView(btnAdd2);
-            listIBTNAdd.add(iIndex, btnAdd2);
+            reLayout.addView(addButton1);
+            AddButList.add(buttonIndex, addButton1);
 
-            Button btnDelete = new Button(CreateActivity.this);
-            btnDelete.setText("DELETE");
-            RelativeLayout.LayoutParams btnDeleteAddParam = new RelativeLayout.LayoutParams(
+            Button deleteButton1 = new Button(CreateActivity.this);
+            deleteButton1.setText("DELETE");
+            RelativeLayout.LayoutParams deleteButtonParam = new RelativeLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            btnDeleteAddParam.setMargins(0, 0, (int) (fDimRatio * 5), 0);
-            btnDeleteAddParam.addRule(RelativeLayout.LEFT_OF, btnIDIndex);
-            btnDelete.setOnClickListener(new View.OnClickListener() {
-
+            deleteButtonParam.setMargins(0, 0, (int) (fDimRatio * 5), 0);
+            deleteButtonParam.addRule(RelativeLayout.LEFT_OF, indexOfAdd);
+            deleteButton1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     deleteContent(v);
                 }
             });
-            rlBtn.addView(btnDelete, btnDeleteAddParam);
-            listIBTNDel.add(iIndex, btnDelete);
+            reLayout.addView(deleteButton1, deleteButtonParam);
+            DelButList.add(buttonIndex, deleteButton1);
 
-            layout.addView(rlBtn);
-
-            EditText etContent1 = new EditText(CreateActivity.this);
+            lLayoutIn.addView(reLayout);
+//--------------------------------------Options-----------------------------------------------------
+            EditText option1 = new EditText(CreateActivity.this);
             LinearLayout.LayoutParams et1Param = new LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            etContent1.setLayoutParams(et1Param);
-            etContent1.setBackgroundColor(Color.argb(255, 255, 255, 255));
-            layout.addView(etContent1);
+            option1.setLayoutParams(et1Param);
+            option1.setBackgroundColor(Color.argb(255, 255, 255, 255));
+            lLayoutIn.addView(option1);
 
-            EditText etContent2 = new EditText(CreateActivity.this);
+            EditText option2 = new EditText(CreateActivity.this);
             LinearLayout.LayoutParams et2Param = new LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            etContent2.setLayoutParams(et2Param);
-            etContent2.setBackgroundColor(Color.argb(255, 255, 255, 255));
-            layout.addView(etContent2);
+            option2.setLayoutParams(et2Param);
+            option2.setBackgroundColor(Color.argb(255, 255, 255, 255));
+            lLayoutIn.addView(option2);
 
-            EditText etContent3 = new EditText(CreateActivity.this);
+            EditText option3 = new EditText(CreateActivity.this);
             LinearLayout.LayoutParams et3Param = new LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            etContent3.setLayoutParams(et3Param);
-            etContent3.setBackgroundColor(Color.argb(255, 255, 255, 255));
-            layout.addView(etContent3);
+            option3.setLayoutParams(et3Param);
+            option3.setBackgroundColor(Color.argb(255, 255, 255, 255));
+            lLayoutIn.addView(option3);
 
-            llContentView.addView(layout, iIndex);
-            btnIDIndex++;
+            lLayoutOut.addView(lLayoutIn, buttonIndex);
+            indexOfAdd++;
         }
     }
 
@@ -167,19 +164,18 @@ public class CreateActivity extends AppCompatActivity {
         if (v == null) {
             return;
         }
-
-        int iIndex = -1;
-        for (int i = 0; i < listIBTNDel.size(); i++) {
-            if (listIBTNDel.get(i) == v) {
-                iIndex = i;
+        int buttonIndex = -1;
+        for (int i = 0; i < DelButList.size(); i++) {
+            if (DelButList.get(i) == v) {
+                buttonIndex = i;
                 break;
             }
         }
 
-        if (iIndex >= 0) {
-            listIBTNAdd.remove(iIndex);
-            listIBTNDel.remove(iIndex);
-            llContentView.removeViewAt(iIndex);
+        if (buttonIndex >= 0) {
+            AddButList.remove(buttonIndex);
+            DelButList.remove(buttonIndex);
+            lLayoutOut.removeViewAt(buttonIndex);
         }
     }
 }
